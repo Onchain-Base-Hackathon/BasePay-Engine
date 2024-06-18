bd73919f94db4c005e2ccb18cd49a5b9dfc0b8a4
mahfuz <mahfuzbello6@gmail.com>
2024-06-18 14:35:55 +0100

    update: fixes

--- src/util/exception.filter.ts
+++ src/util/exception.filter.ts
@@ -1,21 +1,16 @@
 import {
-    ExceptionFilter,
-    Catch,
-    HttpException,
-    ArgumentsHost,
-    Logger,
-  } from '@nestjs/common';
-  import * as Sentry from '@sentry/node';
-  
-  @Catch()
-  export class HttpExceptionFilter implements ExceptionFilter {
-    private readonly logger = new Logger(HttpExceptionFilter.name);
-  
-    catch(exception: HttpException, host: ArgumentsHost) {
-      // Log error to Sentry
-      Sentry.captureException(exception);
-  
-      return exception;
-    }
+  ExceptionFilter,
+  Catch,
+  HttpException,
+  ArgumentsHost,
+  Logger,
+} from '@nestjs/common';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(HttpExceptionFilter.name);
+
+  catch(exception: HttpException, host: ArgumentsHost) {
+    return exception;
   }
-  
+}
